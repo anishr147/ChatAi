ec635e5b7e1d25f95c8a6bd9e2bc66a9bec30031
Anish Kumar <rajanish8340@gmail.com>
2026-07-06 17:53:51 +0530

    Changes in  ARCJET

--- backend/src/lib/arcjet.js
+++ backend/src/lib/arcjet.js
@@ -1,39 +1,19 @@
-import arcjet, { filter ,detectBot, shield, slidingWindow} from "@arcjet/node";
-
-
-
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
 import dotenv from "dotenv";
 dotenv.config();
 
 const aj = arcjet({
-  key:process.env.ARCJET_API_KEY, 
+  key: process.env.ARCJET_API_KEY,
   rules: [
-    // Shield protects your app from common attacks e.g. SQL injection
     shield({ mode: "LIVE" }),
-    filter({
-      allow: [
-          'http.request.user_agent contains "Postman"',
-        'ip.src eq "127.0.0.1"',
-      ],
-      mode: "DRY_RUN", // Blocks requests. Use "DRY_RUN" to log only
-    }),
-    // Create a bot detection rule
     detectBot({
-      mode: "DRY_RUN", // Blocks requests. Use "DRY_RUN" to log only
-      // Block all bots except the following
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        // Uncomment to allow these other common bot categories
-        // See the full list at https://arcjet.com/bot-list
-        //"CATEGORY:MONITOR", // Uptime monitoring services
-        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
-      ],
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"],
     }),
-    // Create a token bucket rate limit. Other algorithms are supported.
     slidingWindow({
-        mode: "DRY_RUN", // Blocks requests. Use "DRY_RUN" to log only
-        max: 100, // Max 100 requests 
-        interval: "1m", 
+      mode: "LIVE",
+      max: 100,
+      interval: "1m",
     }),
   ],
 });
